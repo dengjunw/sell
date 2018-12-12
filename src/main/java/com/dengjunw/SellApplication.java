4f3ca289ed2b526cf0bc013c5d9f5d2008989d9d
dengjunwu <djw1241120422@163.com>
2018-12-12 23:54:20 +0800

    add jdbc conf

--- src/main/java/com/dengjunw/SellApplication.java
+++ src/main/java/com/dengjunw/SellApplication.java
@@ -1,13 +1,21 @@
 package com.dengjunw;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
+@Slf4j
 public class SellApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SellApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(SellApplication.class, args);
+		String[] profiles = context.getEnvironment().getActiveProfiles();
+
+		log.info("{} is active", Arrays.toString(profiles));
 	}
 
 }
